0eafeeb8d5ec92d46f24eb061e59cb4d469ac1f5
Ikaros <327209194@qq.com>
2023-01-27 14:05:12 +0800

    新增linux ubuntu x86_64 py3.8 cpu版本的合成程序和WEB API

---
+++ inference_api.py
@@ -0,0 +1,144 @@
+# win10 py3.8 cuda
+
+import matplotlib
+import matplotlib.pylab as plt
+
+import sys
+
+sys.path.append('waveglow/')
+import numpy as np
+import torch
+
+from hparams import create_hparams
+from model import Tacotron2
+from layers import TacotronSTFT, STFT
+from audio_processing import griffin_lim
+from train import load_model
+from text import text_to_sequence
+from waveglow.denoiser import Denoiser
+
+from scipy.io.wavfile import write
+import os
+
+# 下面这些库用来做web API
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+from pathlib import Path
+import uvicorn
+import time
+
+
+app = FastAPI()
+
+
+@app.get('/tt2/content={content}')
+def tts(content: str = None):
+    # 配置文件存储于 outdir/config.txt，数据文件存储于 outdir/data.txt
+    # config.txt文件内容分别为 checkpoint_path（模型路径），waveglow_path（waveglow模型路径），
+    # data_filename（data文件路径，存放待合成的语句），text_to_sequence_type，output_dir（音频输出路径）
+    my_config = "outdir\config.txt"
+
+    def plot_data(data, figsize=(16, 4)):
+        fig, axes = plt.subplots(1, len(data), figsize=figsize)
+        for i in range(len(data)):
+            axes[i].imshow(data[i], aspect='auto', origin='lower',
+                           interpolation='none')
+
+    hparams = create_hparams()
+    hparams.sampling_rate = 22050
+
+    fo = open(my_config, "a+", encoding='utf-8')
+    print("读取配置文件: ", fo.name)
+    # 重新设置文件读取指针到开头
+    fo.seek(0, 0)
+    checkpoint_path = ""
+    waveglow_path = ""
+    data_filename = ""
+    text_to_sequence_type = ""
+    # 输出音频目录
+    # output_dir = ".\\outdir\\wavs"
+    output_dir = ".\\outdir"
+    i = 0
+    for line in fo.readlines():  # 依次读取每行
+        text = line.strip()
+        if i == 0:
+            checkpoint_path = text
+        elif i == 1:
+            waveglow_path = text
+        elif i == 2:
+            data_filename = text
+        elif i == 3:
+            text_to_sequence_type = text
+        elif i == 4:
+            output_dir = text
+        i = i + 1
+    # 关闭文件
+    fo.close()
+
+    print("checkpoint_path = " + checkpoint_path)
+    print("waveglow_path = " + waveglow_path)
+    print("data_filename = " + data_filename)
+    print("text_to_sequence_type = " + text_to_sequence_type)
+    print("output_dir = " + output_dir)
+
+    # checkpoint_path = "outdir/checkpoint_24000"
+    model = load_model(hparams)
+    model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+    _ = model.cuda().eval()  # .half()
+
+    # waveglow_path = 'waveglow_256channels_ljs_v3.pt'
+    # waveglow_path = '..\waveglow_256channels_universal_v5.pt'
+    waveglow = torch.load(waveglow_path)['model']
+    waveglow.cuda().eval()  # .half()
+    for k in waveglow.convinv:
+        k.float()
+    denoiser = Denoiser(waveglow)
+
+    # data_filename = "outdir\data.txt"
+
+    try:
+        os.makedirs(output_dir)
+    except FileExistsError:
+        print(output_dir + "已存在")
+
+    print("本次合成内容为: %s" % (content))
+
+    # text_to_sequence_type = ['japanese_cleaners', 'japanese_accent_cleaners', 'japanese_phrase_cleaners', 'japanese_tokenization_cleaners']
+    sequence = np.array(text_to_sequence(content, [text_to_sequence_type]))[None, :]
+    sequence = torch.autograd.Variable(
+        torch.from_numpy(sequence)).cuda().long()
+
+    mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+    plot_data((mel_outputs.float().data.cpu().numpy()[0],
+               mel_outputs_postnet.float().data.cpu().numpy()[0],
+               alignments.float().data.cpu().numpy()[0].T))
+
+    with torch.no_grad():
+        audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+    # ipd.Audio(audio[0].data.cpu().numpy(), rate=hparams.sampling_rate)
+
+    audio = denoiser(audio, strength=0.01)[:, 0]
+
+    audio = audio * hparams.sampling_rate
+    audio = audio.squeeze()
+    audio = audio.cpu().numpy()
+    audio = audio.astype('int16')
+    #file_name = "{}.wav".format(content)
+    file_name = "{}.wav".format(str(time.time()))
+    audio_path = os.path.join(output_dir, file_name)
+    write(audio_path, hparams.sampling_rate, audio)
+    print('wav输出路径：' + audio_path)
+
+    # audio_denoised = denoiser(audio, strength=0.01)[:, 0]
+    # ipd.Audio(audio_denoised.cpu().numpy(), rate=hparams.sampling_rate)
+
+    file_path = Path(audio_path)
+    return FileResponse(file_path.as_posix(), filename=file_name)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app,
+                host="0.0.0.0",
+                port=56789,
+                workers=1)
