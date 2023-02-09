067e7dea8a79e578f84b31dae7e6921017588d73
Ikaros <327209194@qq.com>
2023-02-09 08:56:04 +0800

    进一步筛选数据；提高训练的迭代数和batch

--- hparams.py
+++ hparams.py
@@ -17,7 +17,7 @@ class create_hparams():
     ################################
     # Experiment Parameters        #
     ################################
-    epochs = 25
+    epochs = 100
     iters_per_checkpoint = 4000
     seed= 1234
     dynamic_loss_scaling = True
@@ -88,7 +88,7 @@ class create_hparams():
     learning_rate = 1e-3
     weight_decay = 1e-6
     grad_clip_thresh = 1.0
-    batch_size = 2
+    batch_size = 4
     mask_padding = True  # set model's padded outputs to padded values
 
 
