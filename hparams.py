304c4f01773699d1e8df5cf3dae5bc22b631bcbd
Ikaros <327209194@qq.com>
2023-02-13 16:07:18 +0800

    升到60batch，3000检查点，压到colab免费边界

--- hparams.py
+++ hparams.py
@@ -18,7 +18,7 @@ class create_hparams():
     # Experiment Parameters        #
     ################################
     epochs = 1000
-    iters_per_checkpoint = 4000
+    iters_per_checkpoint = 3000
     seed= 1234
     dynamic_loss_scaling = True
     fp16_run = False
@@ -88,7 +88,7 @@ class create_hparams():
     learning_rate = 1e-3
     weight_decay = 1e-6
     grad_clip_thresh = 1.0
-    batch_size = 4
+    batch_size = 60
     mask_padding = True  # set model's padded outputs to padded values
 
 
